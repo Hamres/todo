29956fec078930d0aeeff5c258881af95b2e739f
Hamres <75183238+Hamres@users.noreply.github.com>
2022-08-12 22:47:47 +0300

    fix

--- JS/main.js
+++ JS/main.js
@@ -114,7 +114,7 @@ function checkEmptyList() {
   if (tasks.length === 0) {
     const emptyListHTML = `
       <li id="emptyList" class="list-group-item empty-list">
-      <img src="/img/task.png" alt="Empty" width="48" class="mt-3" />
+      <img src="img/task.png" alt="Empty" width="48" class="mt-3" />
       <div class="empty-list__title">Список дел пуст</div>
       </li>`
     tasksList.insertAdjacentHTML('afterbegin', emptyListHTML)
@@ -140,10 +140,10 @@ function renderTask(task) {
             <span class="${cssClass}">${task.text}</span>
             <div class="task-item__buttons">
                 <button type="button" data-action="done" class="btn-action">
-                     <img src="/img/tick.svg" alt="Done" width="18" height="18" />
+                     <img src="img/tick.svg" alt="Done" width="18" height="18" />
                 </button>
                 <button type="button" data-action="delete" class="btn-action">
-                    <img src="/img/cross.svg" alt="Done" width="18" height="18" />
+                    <img src="img/cross.svg" alt="Done" width="18" height="18" />
                 </button>
             </div>
         </li>`
